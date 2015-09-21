487386ae66543b4635a0fb95872c20c01a495642
benatespina <benatespina@gmail.com>
2015-09-21 10:50:16 +0200

    Updated WPFoundation that supports the new Mailer

--- wp-config-custom-sample.php
+++ wp-config-custom-sample.php
@@ -25,20 +25,18 @@ define('WPLANG',              '');       // '': default (English); 'es_ES': Span
 define('WP_DEBUG',            true);
 define('WP_AUTO_UPDATE_CORE', false);
 
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
+define('AUTH_KEY',          'Put your unique phrase here');
+define('SECURE_AUTH_KEY',   'Put your unique phrase here');
+define('LOGGED_IN_KEY',     'Put your unique phrase here');
+define('NONCE_KEY',         'Put your unique phrase here');
+define('AUTH_SALT',         'Put your unique phrase here');
+define('SECURE_AUTH_SALT',  'Put your unique phrase here');
+define('LOGGED_IN_SALT',    'Put your unique phrase here');
+define('NONCE_SALT',        'Put your unique phrase here');
 
-define('NVL_SMTP_FORCE',      true);                     // Force to use SMTP
-define('NVL_SMTP_FROM_NAME',  'Awesome Name');           // Sender name
-define('NVL_SMTP_FROM_EMAIL', 'info@lin3s.com');         // Sender email
-define('NVL_SMTP_USER',       'info@lin3s.com');         // Gmail user to send email via SMTP
-define('NVL_SMTP_PASS',       'pass');                   // Gmail user password
-define('NVL_SMTP_HOST',       'smtp.mandrillapp.com');
-define('NVL_SMTP_PORT',       587);
-define('NVL_SMTP_SECURE',     'tsl');
+define('MAILER_HOST',       'smtp.mandrillapp.com');
+define('MAILER_PORT',       587);
+define('MAILER_USERNAME',   'Put the username here');
+define('MAILER_PASSWORD',   'Put the password here');
+define('MAILER_FROM',       'The email address that will be shown to the receiver');
+define('MAILER_FROM_NAME',  'The name that will be shown to the receiver');
