728a6a18b20b5b8a36d240be76c13b442616e2ca
benatespina <benatespina@gmail.com>
2016-02-01 10:19:21 +0100

    Enabled fastclick and svg4everybody inside app.js

--- src/themes/WordpressStandard/Resources/assets/js/app.js
+++ src/themes/WordpressStandard/Resources/assets/js/app.js
@@ -1,17 +1,17 @@
 /*
- * This file is part of the WordPress Standard project.
+ * This file is part of the Guggenheim microsites project.
  *
  * Copyright (c) 2015-2016 LIN3S <info@lin3s.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
- * @author Gorka Laucirica <gorka.lauzirika@gmail.com>
- * @author Beñat Espiña <benatespina@gmail.com>
+ * @author Beñat Espiña <bespina@lin3s.com>
  */
 
 'use strict';
 
-(function ($) {
-
-}(jQuery));
+$(function() {
+  FastClick.attach(document.body);
+  svg4everybody();
+});
