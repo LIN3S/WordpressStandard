4bd8609d924886adbbd0840c6870ea72c585e76d
benatespina <benatespina@gmail.com>
2016-05-10 11:33:57 +0200

    Renamed tagManager to googleTagManager

--- src/themes/WordpressStandard/Resources/assets/js/cookies.js
+++ src/themes/WordpressStandard/Resources/assets/js/cookies.js
@@ -17,30 +17,30 @@
   var
     allLinks = 'a, button, .cookies__actions .button',
     $cookies = $('.cookies'),
-    $tagManager = $('.tag-manager'),
+    $googleTagManager = $('.google-tag-manager'),
     $window = $(window),
     scrollTop = 400;
 
   function addGoogleTagManager() {
-    if (typeof tagManagerId === 'undefined') {
-      console.log('Please define tagManagerId global JS variable to append GTM code');
+    if (typeof googleTagManagerId === 'undefined') {
+      console.log('Please define googleTagManagerId global JS variable to append GTM code');
 
       return;
     }
 
-    $tagManager.append(
-      '<noscript><iframe src="//www.googletagmanager.com/ns.html?id=' + tagManagerId + '"' +
+    $googleTagManager.append(
+      '<noscript><iframe src="//www.googletagmanager.com/ns.html?id=' + googleTagManagerId + '"' +
       'height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>' +
       '<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({"gtm.start":' +
       'new Date().getTime(),event:"gtm.js"});var f=d.getElementsByTagName(s)[0],' +
       'j=d.createElement(s),dl=l!="dataLayer"?"&l="+l:"";j.async=true;j.src=' +
       '"//www.googletagmanager.com/gtm.js?id="+i+dl;f.parentNode.insertBefore(j,f);' +
-      '})(window,document,"script","dataLayer","' + tagManagerId + '");</script>'
+      '})(window,document,"script","dataLayer","' + googleTagManagerId + '");</script>'
     );
   }
 
   function removeGoogleTagManager() {
-    $tagManager.empty();
+    $googleTagManager.empty();
   }
 
   function setCookie(name, value, expirationDays) {
