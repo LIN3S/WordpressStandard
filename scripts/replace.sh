15801b0030cc84ff38b6e0483394877a455c1454
benatespina <benatespina@gmail.com>
2016-05-19 08:43:08 +0200

    Added license header

--- scripts/replace.sh
+++ scripts/replace.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# This file is part of the WordPress Standard project.
+#
+# Copyright (c) 2015-2016 LIN3S <info@lin3s.com>
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+#
+# @author Jon Torrado <jontorrado@gmail.com>
+
 echo -e "Enter the name of your project (one word, first letter uppercase): \c "
 read project
 lowerproject="${project,,}"
