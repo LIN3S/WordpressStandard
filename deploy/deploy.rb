53565064198ff5ae9cdf4e9ad4587cbf30d4b3fd
gorkalaucirica <gorka.lauzirika@gmail.com>
2016-06-21 12:57:32 +0200

    Escape user and password on mysqldump command for database:download task in deploy.rb
    We had issues with a password with '|' character

--- deploy/deploy.rb
+++ deploy/deploy.rb
@@ -152,7 +152,7 @@ namespace :database do
         end
       end
       if dbuser != nil and dbpass != nil and dbname != nil
-        execute "cd #{shared_path};mysqldump -u#{dbuser} -p#{dbpass} #{dbname} > #{dbname}_cap.sql"
+        execute "cd #{shared_path};mysqldump -u'#{dbuser}' -p'#{dbpass}' #{dbname} > #{dbname}_cap.sql"
         download! "#{shared_path}/#{dbname}_cap.sql", "."
         execute :rm, "-f", "#{shared_path}/#{dbname}_cap.sql"
       else
